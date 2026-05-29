c67231c6e892eecc0668b6cf0cf9ed4e1ba97bdd
iebunie5 <iebunie5@gmail.com>
2026-05-29 13:56:55 +0900

    Make kiosk startup use local serve binary

--- start-buyeo-story.bat
+++ start-buyeo-story.bat
@@ -7,13 +7,23 @@ if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
 
 cd /d "%ROOT%"
 
+if not exist "%ROOT%node_modules\.bin\serve.cmd" (
+  echo [%date% %time%] serve package missing. Running npm install...>> "%LOG_DIR%\startup.log"
+  call npm.cmd install
+)
+
+if not exist "%ROOT%out\index.html" (
+  echo [%date% %time%] out folder missing. Running npm run build...>> "%LOG_DIR%\startup.log"
+  call npm.cmd run build
+)
+
 echo [%date% %time%] Starting Buyeo AI Story proxy...>> "%LOG_DIR%\startup.log"
 start "Buyeo Story Proxy" /min /D "%ROOT%story-proxy" node server.js
 
 timeout /t 3 /nobreak > nul
 
 echo [%date% %time%] Starting Buyeo AI Story app...>> "%LOG_DIR%\startup.log"
-start "Buyeo Story App" /min /D "%ROOT%" npm.cmd run serve:out
+start "Buyeo Story App" /min /D "%ROOT%" "%ROOT%node_modules\.bin\serve.cmd" out -l tcp://127.0.0.1:3000
 
 timeout /t 8 /nobreak > nul
 
